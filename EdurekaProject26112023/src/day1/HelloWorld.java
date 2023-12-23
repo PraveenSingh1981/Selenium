76a51bd322d1229426960adc0ccaa52b9f550a4c
prave <prave@DESKTOP-2M1LJRO>
2023-12-23 23:22:16 +0000

    Swith case added

--- EdurekaProject26112023/src/day1/HelloWorld.java
+++ EdurekaProject26112023/src/day1/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
+		String myfirstname = "My Name is Praveen";
+				
+		System.out.println(myfirstname);
 	}
 
 }
---
+++ EdurekaProject26112023/src/day2/SwtichCase.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class SwtichCase {
+
+	public static void main(String[] args) {
+		
+		int numberOfday = 1;
+		switch (numberOfday) {
+		case 1:
+			System.out.println("Sunday");
+			
+			break;
+
+		default:
+			System.out.println("Invalid Day");
+			break;
+		}
+
+	}
+
+}
